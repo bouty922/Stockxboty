33c46ebde728db4f792148ec7dd9293f1a021fba
Bouty922 <jeremy.taverne@outlook.com>
2018-08-18 20:04:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -98,4 +98,4 @@ async def on_message(message):
         await client.send_message(message.channel, embed=embed)
 
 if __name__ == '__main__':
-    client.run(process.env.discord_token);
+    client.login(process.env.discord_token);
